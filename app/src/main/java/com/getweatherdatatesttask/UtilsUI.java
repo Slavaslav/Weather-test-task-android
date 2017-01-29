c1327901cd3ae6dcd17e74e8d5909993a9dc3276
awecea <awecea@gmail.com>
2017-01-29 16:41:24 +0200

    inspection delete enum RequestType from HttpRequestClient.java

--- app/src/main/java/com/getweatherdatatesttask/UtilsUI.java
+++ app/src/main/java/com/getweatherdatatesttask/UtilsUI.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
-public class UtilsUI {
+class UtilsUI {
 
     public static void hideSoftKeyboard(MapsActivity activity) {
         InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
